cb3412b6a2a6e7b26597cbfc026c0538d8ef1385
Ted Hart-Davis <tedster@tedster.net>
2018-07-30 18:13:16 -0700

    comment out powerset demo

--- royalflush.py
+++ royalflush.py
@@ -6,7 +6,7 @@ from statistics import mean
 
 # https://docs.python.org/2.7/library/itertools.html#recipes
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    #"powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
