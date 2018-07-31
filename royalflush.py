24d2e6e463e157a31e332bcdb4f217c3675a1fb4
Ted Hart-Davis <tedster@tedster.net>
2018-07-30 18:10:28 -0700

    remove old and new printhand: not used

--- royalflush.py
+++ royalflush.py
@@ -10,14 +10,6 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-"""def printHand(hand):
-    [print(card) for card in hand]
-    print()"""
-
-def printHand(hand):
-    for card in hand:
-        print hand
-
 def drawInitial(deck):
     return deck.draw(5)
 
